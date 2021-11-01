a9f54d625183ebc2c4c8ee015ffbc9f47380e906
tech <tech@macbook-pro-de-tech.home>
2021-11-01 16:18:30 +0100

    some cleaning to manage files better

--- so_long.c
+++
@@ -1,7 +0,0 @@
-#include "so_long.h"
-
-int	main(int argc, char	**argv)
-{
-	open(); // the path to the .ber map will be recieved as arg
-	// send the fd to parser and start parsing the map with GNL
-}
