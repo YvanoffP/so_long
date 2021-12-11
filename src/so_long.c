7094a661bc41be7663526548e30a9bd738ecddff
tech <tech@macbook-pro-de-tech.home>
2021-12-11 16:50:24 +0100

    fuck mlx

--- src/so_long.c
+++ src/so_long.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tpauvret <marvin@42lausanne.ch>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/20 14:13:44 by tpauvret          #+#    #+#             */
-/*   Updated: 2021/11/22 15:30:27 by tpauvret         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "so_long.h"
 
 int	main(int argc, char **argv)
@@ -17,22 +5,19 @@ int	main(int argc, char **argv)
 	char	**tab;
 	void	*mlx;
 	void	*mlx_win;
+	void	*img;
 	t_map	map;
-	int x = 0, y = 0;;
+	int x = 100, y = 300;
+	char *relative_path = "img/wall.xpm";
 
 	tab = parsing_map(argc, argv);
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Bima");
-	map.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, 1024, 720, "so_long");
+	map.img = mlx_new_image(mlx, 1024, 720);
 	map.addr = mlx_get_data_addr(map.img, &map.bits_pixel, &map.line_length, &map.endian);
-	while (x++ < 100)
-	{
-		y = 0;
-		while (y++ < 100)
-				my_mlx_pixel_put(&map, x, y, 0x00FF0000);
-		my_mlx_pixel_put(&map, x, y, 0x00FF0000);
-	}
-	mlx_put_image_to_window(mlx, mlx_win, map.img, 0, 0);
+	img = mlx_xpm_file_to_image(mlx, relative_path, &x, &y);
+	printf("%s\n", img);
+	
 	mlx_loop(mlx);
 	return (0);
 }
