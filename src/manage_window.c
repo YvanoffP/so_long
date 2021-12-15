936145cb9875e2fa22223889fc7638052663535e
tech <tech@macbook-pro-de-tech.home>
2021-12-15 22:47:04 +0100

    aled

--- src/manage_window.c
+++ src/manage_window.c
@@ -4,7 +4,6 @@ void	my_mlx_pixel_put(t_img *sprite, int x, int y, int color)
 {
 	char	*dst;
 
-	//if (x && y) en gros, si x ou t vaut 0 on multiplie par 1 et pas par 0
 	dst = sprite->addr + (y * sprite->line_length + x * (sprite->bits_pixel / 8));
 	*(unsigned int*)dst = color;
-}	iii
+}
